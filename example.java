7ac146b3904ee46926976b2f0f1b33f9e638472a
deekshitharavi <40123683+deekshitharavi@users.noreply.github.com>
2018-06-09 22:29:28 +0530

    first code in java

---
+++ example.java
@@ -0,0 +1,22 @@
+class compute
+{
+   public void disp()
+   {
+     System.out.println(" hello world");
+   }
+   
+ 
+}
+class example
+{
+
+  public static void main(String a[])
+  {
+  compute c= new compute();
+   c.disp();
+    
+   }
+   
+
+ 
+}
